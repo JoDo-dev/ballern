2a856a1a0f5ecbbae7d09d092387c22d7a56855e
JoDo-dev <54305854+JoDo-dev@users.noreply.github.com>
2020-02-04 21:38:41 +0100

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <component :is="state" />
+  </div>
+</template>
+
+<script>
+import Home from './components/home/home.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Home,
+  },
+  data() {
+    return {
+      state: 'Home',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/home/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{msg}}</h1>
+</template>
+
+<script>
+
+export default {
+
+  name: 'Home',
+  data() {
+    return {
+      msg: 'Some Goodies',
+    };
+  },
+
+};
+</script>
